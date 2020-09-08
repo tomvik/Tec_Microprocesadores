068a1b65916b0fe9417d0247052ecd0e5de87543
Tomas Lugo <lugo.lbmi@gmail.com>
2020-09-08 15:29:20 -0500

    - Adds comments to functions

--- Hw2/ArgumentsCheck.h
+++ Hw2/ArgumentsCheck.h
@@ -7,8 +7,11 @@
 
 namespace ArgumentsCheck {
 
+// Enum class for the possible argument cases.
 enum class ArgumentsCase { kOk = 0, kHelp = 1, kWrongArguments = 2 };
 
+// Checks if the arguments are correct, and prints the appropiate message if neccesary.
+// It also updates the value of the variables real_dimension and real_num_threads.
 ArgumentsCase handleArguments(int* real_dimension, int* real_num_threads, int argc,
                               char** argv);
 
--- Hw2/Timer.h
+++ Hw2/Timer.h
@@ -9,16 +9,21 @@ namespace Timer {
 
 class Timer {
  public:
+    // Creates a timer with the name given.
     explicit Timer(const std::string& name);
+
+    // Destructor of the timer, which will also print out the amount of time it went
+    // since its construction.
     ~Timer();
 
  private:
+    // Returns the duration since the initial time in seconds.
     const double getDuration();
 
     std::string name_;
     std::chrono::_V2::system_clock::time_point initial_time_;
 };
-    
-} // namespace Timer
+
+}  // namespace Timer
 
 #endif  // HW2_TIMER_H_
