2b1a94e558284f24b78c06d21e503019e463392d
Tomas Lugo <lugo.lbmi@gmail.com>
2020-10-20 12:58:16 -0500

    Make it modular size

--- Hw6/src/main.cpp
+++ Hw6/src/main.cpp
@@ -18,108 +18,117 @@
 
 #include <iostream>
 
-void sequencial(const float A[16], const float B[16], float C[16], float D[16], float E[16],
-                float F[16]) {
+constexpr int kArraySize = 16;
+
+void sequencial(const float A[kArraySize], const float B[kArraySize], float C[kArraySize],
+                float D[kArraySize], float E[kArraySize], float F[kArraySize]) {
     Timer::Timer timer("Sequencial");
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < kArraySize; ++i) {
         C[i] = A[i] + B[i];
         D[i] = A[i] - B[i];
         E[i] = A[i] * B[i];
         F[i] = A[i] / B[i];
     }
     printf("C= ");
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < kArraySize; ++i) {
         printf("%.2f ", C[i]);
     }
     printf("\n");
     printf("D= ");
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < kArraySize; ++i) {
         printf("%.2f ", D[i]);
     }
     printf("\n");
     printf("E= ");
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < kArraySize; ++i) {
         printf("%.2f ", E[i]);
     }
     printf("\n");
     printf("F= ");
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < kArraySize; ++i) {
         printf("%.2f ", F[i]);
     }
     printf("\n");
 }
 
-void OMP_Sections(const float A[16], const float B[16], float C[16], float D[16], float E[16],
-                  float F[16]) {
+void OMP_Sections(const float A[kArraySize], const float B[kArraySize], float C[kArraySize],
+                  float D[kArraySize], float E[kArraySize], float F[kArraySize]) {
     Timer::Timer timer("OMP_Sections");
     int i = 0;
-#pragma omp parallel num_threads(4) shared(A, B, C, D, E, F) private(i)
+#pragma omp parallel num_threads(4) shared(C, D, E, F) private(i)  // nowait
     {
 #pragma omp sections nowait
         {
 #pragma omp section
             {
                 printf("C thread: %d\n", omp_get_thread_num());
-                for (i = 0; i < 16; ++i) {
+                for (i = 0; i < kArraySize; ++i) {
                     C[i] = A[i] + B[i];
                 }
-            }
+            }  // End section.
 #pragma omp section
             {
                 printf("D thread: %d\n", omp_get_thread_num());
-                for (i = 0; i < 16; ++i) {
+                for (i = 0; i < kArraySize; ++i) {
                     D[i] = A[i] - B[i];
                 }
-            }
+            }  // End section.
 #pragma omp section
             {
                 printf("E thread: %d\n", omp_get_thread_num());
-                for (i = 0; i < 16; ++i) {
+                for (i = 0; i < kArraySize; ++i) {
                     E[i] = A[i] * B[i];
                 }
-            }
+            }  // End section.
 #pragma omp section
             {
                 printf("F thread: %d\n", omp_get_thread_num());
-                for (i = 0; i < 16; ++i) {
+                for (i = 0; i < kArraySize; ++i) {
                     F[i] = A[i] / B[i];
                 }
-            }
-        }
-    }
+            }  // End section.
+        }      // End sections
+
+    }  // End Parallel
+
     printf("C= ");
-    for (i = 0; i < 16; ++i) {
+    for (i = 0; i < kArraySize; ++i) {
         printf("%.2f ", C[i]);
     }
     printf("\n");
     printf("D= ");
-    for (i = 0; i < 16; ++i) {
+    for (i = 0; i < kArraySize; ++i) {
         printf("%.2f ", D[i]);
     }
     printf("\n");
     printf("E= ");
-    for (i = 0; i < 16; ++i) {
+    for (i = 0; i < kArraySize; ++i) {
         printf("%.2f ", E[i]);
     }
     printf("\n");
     printf("F= ");
-    for (i = 0; i < 16; ++i) {
+    for (i = 0; i < kArraySize; ++i) {
         printf("%.2f ", F[i]);
     }
     printf("\n");
 }
 
 int main() {
-    const float A[16] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
-    const float B[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    float C[16] = {0};
-    float D[16] = {0};
-    float E[16] = {0};
-    float F[16] = {0};
+    float A[kArraySize] = {0};
+    float B[kArraySize] = {0};
+    float C[kArraySize] = {0};
+    float D[kArraySize] = {0};
+    float E[kArraySize] = {0};
+    float F[kArraySize] = {0};
+
+    for (int i = 0; i < kArraySize; ++i) {
+        A[i] = 10 + i;
+        B[i] = 1 + i;
+    }
 
     sequencial(A, B, C, D, E, F);
 
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < kArraySize; ++i) {
         C[i] = 0;
         D[i] = 0;
         E[i] = 0;
