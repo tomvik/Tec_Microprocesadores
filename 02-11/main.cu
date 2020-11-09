d856e17bf82d0c7cebf19cb3128387504afe469c
Tomas Lugo <lugo.lbmi@gmail.com>
2020-11-09 13:06:59 -0600

    another class

--- 02-11/main.cu
+++ 02-11/main.cu
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 __global__ 
-void cuda_hello() { printf("Hello World from GPU! %d\n", threadIdx.x); }
+void HelloThreadIdx() { printf("Hello World from GPU! %d %d\n", blockIdx.x, threadIdx.x); }
+
+__global__ 
+void HelloBlockThreadIdx() { printf("Hello World from GPU! %d %d\n", blockIdx.x, threadIdx.x); }
 
 int main() {
-    cuda_hello<<<2, 4>>>();
+    HelloThreadIdx<<<2, 4>>>();
+    HelloBlockThreadIdx<<<2, 4>>>();
     cudaDeviceSynchronize();
     return 0;
 }
