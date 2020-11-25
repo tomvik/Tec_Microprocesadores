9d81460e293ecda71901b93181028d82ad492b52
Ernesto Cervantes <ernesto.acacej@gmail.com>
2020-11-25 16:19:56 -0600

    HelloThreadIdx

--- proyecto_final/src/main.cpp
+++ proyecto_final/src/main.cpp
@@ -37,10 +37,13 @@
 #include <vector>
 
 template <typename T>
-void printMatrix(const std::vector<std::vector<T>>& matrix, const std::string name) {
+void printMatrix(const std::vector<std::vector<T>> &matrix, const std::string name)
+{
     std::cout << "Printing matrix: " << name << "\n";
-    for (const auto& row : matrix) {
-        for (const auto& element : row) {
+    for (const auto &row : matrix)
+    {
+        for (const auto &element : row)
+        {
             std::cout << element << " ";
         }
         std::cout << "\n";
@@ -49,10 +52,13 @@ void printMatrix(const std::vector<std::vector<T>>& matrix, const std::string na
     std::cout << std::endl;
 }
 
-void printMatrix(double** matrix, const std::pair<int, int> dimensions, const std::string name) {
+void printMatrix(double **matrix, const std::pair<int, int> dimensions, const std::string name)
+{
     std::cout << "Printing matrix: " << name << "\n";
-    for (int i = 0; i < dimensions.first; ++i) {
-        for (int j = 0; j < dimensions.second; ++j) {
+    for (int i = 0; i < dimensions.first; ++i)
+    {
+        for (int j = 0; j < dimensions.second; ++j)
+        {
             std::cout << matrix[i][j] << " ";
         }
         std::cout << "\n";
@@ -61,13 +67,15 @@ void printMatrix(double** matrix, const std::pair<int, int> dimensions, const st
     std::cout << std::endl;
 }
 
-void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier*>& multipliers,
-                const int runs) {
+void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier *> &multipliers,
+                const int runs)
+{
     const int spacing = 30;
 
     std::cout << "\n\n\n\n";
 
-    for (int i = 0; i < (spacing * (multipliers.size() + 1)) + multipliers.size(); ++i) {
+    for (int i = 0; i < (spacing * (multipliers.size() + 1)) + multipliers.size(); ++i)
+    {
         std::cout << "-";
     }
     std::cout << "\n";
@@ -77,7 +85,8 @@ void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier*>& multipli
     std::cout << std::setprecision(10);
 
     std::cout << "|" << std::setw(spacing) << "Corrida";
-    for (int i = 0; i < multipliers.size(); ++i) {
+    for (int i = 0; i < multipliers.size(); ++i)
+    {
         std::string output_msg = multipliers[i]->getMethodName() + " with " +
                                  std::to_string(multipliers[i]->getThreadsAmount()) + " thread(s)";
 
@@ -86,9 +95,11 @@ void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier*>& multipli
 
     std::cout << "\n";
 
-    for (int run = 1; run <= runs; ++run) {
+    for (int run = 1; run <= runs; ++run)
+    {
         std::cout << "|" << std::setw(spacing) << run;
-        for (int multiplier = 0; multiplier < multipliers.size(); ++multiplier) {
+        for (int multiplier = 0; multiplier < multipliers.size(); ++multiplier)
+        {
             std::cout << "|" << std::setw(spacing)
                       << multipliers[multiplier]->getRunTimes()[run - 1];
         }
@@ -97,7 +108,8 @@ void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier*>& multipli
 
     std::cout << "|" << std::setw(spacing) << "Promedio";
 
-    for (int i = 0; i < multipliers.size(); ++i) {
+    for (int i = 0; i < multipliers.size(); ++i)
+    {
         std::cout << "|" << std::setw(spacing) << multipliers[i]->getAverageRunTime();
     }
 
@@ -106,14 +118,16 @@ void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier*>& multipli
     std::cout << "|" << std::setw(spacing) << "% vs Serial";
 
     const double serial_prom =
-        multipliers[0]->getAverageRunTime() + 0.000000000001;  // to avoid division by 0
+        multipliers[0]->getAverageRunTime() + 0.000000000001; // to avoid division by 0
 
     double best_score = 1;
     int best_id = 0;
 
-    for (int i = 0; i < multipliers.size(); ++i) {
+    for (int i = 0; i < multipliers.size(); ++i)
+    {
         const double current_score = multipliers[i]->getAverageRunTime() / serial_prom;
-        if (current_score < best_score) {
+        if (current_score < best_score)
+        {
             best_score = current_score;
             best_id = i;
         }
@@ -122,7 +136,8 @@ void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier*>& multipli
 
     std::cout << "\n";
 
-    for (int i = 0; i < (spacing * (multipliers.size() + 1)) + multipliers.size(); ++i) {
+    for (int i = 0; i < (spacing * (multipliers.size() + 1)) + multipliers.size(); ++i)
+    {
         std::cout << "-";
     }
     std::cout << "\n";
@@ -133,66 +148,70 @@ void printTable(const std::vector<MatrixMultiplier::MatrixMultiplier*>& multipli
     std::cout << "Thanks for using our program :) \n";
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     const ScopeTimer::ScopeTimer timer("Main function");
 
     std::vector<std::ifstream> input_files(2);
     std::string output_file_path = "";
 
-    const auto& argument_case = ArgumentsCheck::handleArgumentsAndGetFileHandles(
+    const auto &argument_case = ArgumentsCheck::handleArgumentsAndGetFileHandles(
         argc, argv, &input_files, &output_file_path);
-    switch (argument_case) {
-        case ArgumentsCheck::ArgumentsCase::kHelp:
-            return 0;
-        case ArgumentsCheck::ArgumentsCase::kWrongArguments:
-            return 1;
-        case ArgumentsCheck::ArgumentsCase::kWrongPathOrFile:
-            return 2;
-        case ArgumentsCheck::ArgumentsCase::kOk:
-            break;
-        default:
-            return 3;
+    switch (argument_case)
+    {
+    case ArgumentsCheck::ArgumentsCase::kHelp:
+        return 0;
+    case ArgumentsCheck::ArgumentsCase::kWrongArguments:
+        return 1;
+    case ArgumentsCheck::ArgumentsCase::kWrongPathOrFile:
+        return 2;
+    case ArgumentsCheck::ArgumentsCase::kOk:
+        break;
+    default:
+        return 3;
     }
 
     std::vector<std::pair<int, int>> dimensions;
 
-    const auto& matrix_input_case = MatrixCheck::handleMatrixInput(&dimensions);
+    const auto &matrix_input_case = MatrixCheck::handleMatrixInput(&dimensions);
 
-    switch (matrix_input_case) {
-        case MatrixCheck::MatrixCase::kOk:
-            break;
-        case MatrixCheck::MatrixCase::kWrongDimensions:
-            return 4;
-        default:
-            return 5;
+    switch (matrix_input_case)
+    {
+    case MatrixCheck::MatrixCase::kOk:
+        break;
+    case MatrixCheck::MatrixCase::kWrongDimensions:
+        return 4;
+    default:
+        return 5;
     }
 
-    const int64_t len_a = sizeof(double*) * dimensions[0].first +
+    const int64_t len_a = sizeof(double *) * dimensions[0].first +
                           sizeof(double) * dimensions[0].second * dimensions[0].first;
-    const int64_t len_b = sizeof(double*) * dimensions[1].first +
+    const int64_t len_b = sizeof(double *) * dimensions[1].first +
                           sizeof(double) * dimensions[1].second * dimensions[1].first;
-    const int64_t len_c = sizeof(double*) * dimensions[2].first +
+    const int64_t len_c = sizeof(double *) * dimensions[2].first +
                           sizeof(double) * dimensions[2].second * dimensions[2].first;
 
-    double** matrix_a = reinterpret_cast<double**>(malloc(len_a));
-    double** matrix_b = reinterpret_cast<double**>(malloc(len_b));
-    double** matrix_c = reinterpret_cast<double**>(malloc(len_c));
+    double **matrix_a = reinterpret_cast<double **>(malloc(len_a));
+    double **matrix_b = reinterpret_cast<double **>(malloc(len_b));
+    double **matrix_c = reinterpret_cast<double **>(malloc(len_c));
 
-    const auto& matrix_malloc_case =
+    const auto &matrix_malloc_case =
         MatrixCheck::handleMallocAndFilling(matrix_a, matrix_b, matrix_c, &input_files, dimensions);
 
-    switch (matrix_malloc_case) {
-        case MatrixCheck::MatrixCase::kOk:
-            break;
-        case MatrixCheck::MatrixCase::kNotEnoughMemory:
-            return 6;
-        case MatrixCheck::MatrixCase::kNotEnoughLines:
-            return 7;
-        default:
-            return 8;
+    switch (matrix_malloc_case)
+    {
+    case MatrixCheck::MatrixCase::kOk:
+        break;
+    case MatrixCheck::MatrixCase::kNotEnoughMemory:
+        return 6;
+    case MatrixCheck::MatrixCase::kNotEnoughLines:
+        return 7;
+    default:
+        return 8;
     }
 
-    std::vector<MatrixMultiplier::MatrixMultiplier*> multipliers;
+    std::vector<MatrixMultiplier::MatrixMultiplier *> multipliers;
 
     multipliers.emplace_back(new MatrixMultiplier::SingleThreadMultiplier());
     multipliers.emplace_back(new MatrixMultiplier::OMPMultiplier(16));
@@ -202,12 +221,14 @@ int main(int argc, char** argv) {
 
     const int runs = 5;
 
-    for (int i = 0; i < multipliers.size(); ++i) {
+    for (int i = 0; i < multipliers.size(); ++i)
+    {
         multipliers[i]->multiplyNTimes(matrix_a, matrix_b, matrix_c, dimensions, runs,
                                        output_file_path);
     }
 
     printTable(multipliers, runs);
+    GPUMatrix::HelloThreadIdx();
 
     free(matrix_a);
     free(matrix_b);
