2c956822d2659bc61dd5bb0fd43381b4292753ff
Ernesto Cervantes <ernesto.acacej@gmail.com>
2020-11-25 21:48:55 -0600

    pointers

--- proyecto_final/src/GPUMatrix.cu
+++ proyecto_final/src/GPUMatrix.cu
@@ -3,15 +3,15 @@
 #include <vector>
 #include <utility> 
 namespace {
-    __global__ void mul(double* A, double* B, double* C, int m, int n, int* colA) {
+    __global__ void mul(double* A, double* B, double* C, int m, int n, int colA) {
         // printf("Hello World from GPU! %d %d\n", blockIdx.x, threadIdx.x);
         int row = blockIdx.y * blockDim.y + threadIdx.y;
         int col = blockIdx.x * blockDim.x + threadIdx.x;
 
         double sum = 0.0;
         if((row < m) && (col < n)) {
-            for(int i = 0; i < *colA; i++) {
-                sum += A[*colA * row + i] * B[col + i * n];
+            for(int i = 0; i < colA; i++) {
+                sum += A[colA * row + i] * B[col + i * n];
             }
             C[row * n + col] = sum;
         }
