ef811c9a50c0d5a92cad4c35419c6a46716c4cc9
Ernesto Cervantes <ernesto.acacej@gmail.com>
2020-11-25 21:38:16 -0600

    update

--- proyecto_final/src/GPUMatrix.cu
+++ proyecto_final/src/GPUMatrix.cu
@@ -2,9 +2,7 @@
 #include <stdio.h>
 
 namespace {
-    __global__
-    // void mul() {
-    void mul(double* A, double* B, double* A, int m, int n, int* colA) {
+    __global__ void mul(double* A, double* B, double* A, int m, int n, int* colA) {
         // printf("Hello World from GPU! %d %d\n", blockIdx.x, threadIdx.x);
         int row = blockIdx.y * blockDim.y + threadIdx.y;
         int col = blockIdx.x * blockDim.x + threadIdx.x;
