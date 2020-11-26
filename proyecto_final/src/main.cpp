7737f30a1c8e668d20d0c7062589431515210889
Ernesto Cervantes <ernesto.acacej@gmail.com>
2020-11-25 23:02:23 -0600

    primero

--- proyecto_final/src/main.cpp
+++ proyecto_final/src/main.cpp
@@ -215,9 +215,9 @@ int main(int argc, char **argv)
 
     std::vector<MatrixMultiplier::MatrixMultiplier *> multipliers;
 
+    multipliers.emplace_back(new MatrixMultiplier::OMPMultiplier(16));
     multipliers.emplace_back(new MatrixMultiplier::CUDAMultiplier());
     multipliers.emplace_back(new MatrixMultiplier::SingleThreadMultiplier());
-    multipliers.emplace_back(new MatrixMultiplier::OMPMultiplier(16));
     multipliers.emplace_back(new MatrixMultiplier::PThreadMultiplier(16));
 
     const int runs = 5;
